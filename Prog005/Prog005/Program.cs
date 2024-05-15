b55b8ca3a402b5e12b7deafb2085814b7c581fcc
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-16 01:00:01 +0300

    add task №5

---
+++ Prog005/Prog005/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace HelloWorld
+{
+
+
+    /*
+    class MyClass
+    {
+        //--.
+        public static void MyMethod()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+        }
+    }
+    */
+
+    //-- Программа расчёта площади равностороннего треугольника, периметр которого известен
+
+    public class Program
+    {
+        //--.
+        public static void Main()
+        {
+
+            string sPerimetr, sStorona;
+            double rPerimetr, rStorona;
+            double rS;
+
+            //--.
+            Console.WriteLine("Please enter Perimetr value");
+            sPerimetr = Console.ReadLine();
+            rPerimetr = Double.Parse(sPerimetr);
+            Console.WriteLine("Please enter Storona value");
+            sStorona = Console.ReadLine();
+            rStorona = Double.Parse(sStorona);
+            
+            //--.
+            rS = Math.Sqrt(rPerimetr * (rPerimetr - rStorona) * (rPerimetr - rStorona) * (rPerimetr - rStorona));
+
+            Console.WriteLine("\tSTORONA \t\tPLOSHAD");
+            Console.WriteLine("\t  {0:F2} \t\t {1:F2}", rStorona, rS);
+        }
+    }
+}
