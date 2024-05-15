0007f4571a72624b09317ec8c3f49fb6bb0dbfe4
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-16 00:11:12 +0300

    task1, task2
    -task 1
    -task 2

---
+++ MyDemoConsoleApplication/MyDemoConsoleApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace HelloWorld
+{
+    class MyClass
+    {
+        //--.
+        public static void MyMethod()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+        }
+    }
+
+
+    public class Program
+    {
+        //--.
+        public static void Main()
+        {
+            //--.
+            MyClass.MyMethod();
+            //--.
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Pro01/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+// See https://aka.ms/new-console-template for more information
+
+
+namespace Pro01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World! Vova 1234");
+			Console.ReadLine();
+		}
+	}
+}
