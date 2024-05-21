02687d93d3eff7be08cc594c8e6f94bd503485d6
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-21 23:24:37 +0300

    add ex.4 work with exceptions
    - упражнение 4. Добавление в C#-программу обработчика исключительных ситуаций.
    - создал новую программу;
    - написал код, запрашивающий у пользователя два целых числа;

---
+++ Prog004/Prog004/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DivideItNameSpace
+{
+    public class DivideIt
+    {
+        //--.
+        public static void Main()
+        {
+            //--.
+            Console.WriteLine("Please enter FIRST value: ");
+            string temp = Console.ReadLine();
+            int i = Int32.Parse(temp);
+            Console.WriteLine("Please enter SECOND value: ");
+            temp = Console.ReadLine();
+            int j = Int32.Parse(temp);
+        }
+    }
+}
